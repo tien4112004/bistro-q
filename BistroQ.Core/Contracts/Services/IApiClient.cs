07655fab96502300a2b8f1595de6c85dd4dcbfa5
Tondeptrai23 <22120375@student.hcmus.edu.vn>
2024-12-02 15:07:35 +0700

    Delete old projects

--- BistroQ.Core/Contracts/Services/IApiClient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BistroQ.Core.Contracts.Services;
-
-/// <summary>
-/// This interface is used to define the secure API client.
-/// </summary>
-public interface IApiClient : IBaseApiClient
-{
-    //Empty
-}
