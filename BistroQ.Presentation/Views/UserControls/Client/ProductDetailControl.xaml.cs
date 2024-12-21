d901b1b0701a3808ed67054c626455158c2b5b2e
Thanh Tien Phan <tien4112004@gmail.com>
2024-12-21 21:20:51 +0700

    Change livechart package

--- BistroQ.Presentation/Views/UserControls/Client/ProductDetailControl.xaml.cs
+++ BistroQ.Presentation/Views/UserControls/Client/ProductDetailControl.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using BistroQ.Presentation.ViewModels.Models;
 using LiveChartsCore;
-using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.SkiaSharpView;
 using Microsoft.UI.Xaml.Controls;
 
 namespace BistroQ.Presentation.Views.UserControls.Client;
@@ -34,10 +34,17 @@ public sealed partial class ProductDetailControl : UserControl
             //    series.MaxRadialColumnWidth = 60;
             //});
 
-            Series = new[] { 2, 4, 1, 4, 3 }.AsPieSeries((value, series) =>
-    {
-        series.MaxRadialColumnWidth = 60;
-    });
+            //        Series = new[] { 2, 4, 1, 4, 3 }.AsPieSeries((value, series) =>
+            //{
+            //    series.MaxRadialColumnWidth = 60;
+            //});
+            Series = new ISeries[]
+            {
+                new PieSeries<double> { Values = new double[] { double.TryParse(nutritionFact?.Calories, out var calories) ? calories : 0 }, Name = "Calories" },
+                new PieSeries<double> { Values = new double[] { double.TryParse(nutritionFact?.Fat, out var fat) ? fat : 0 }, Name = "Fat" },
+                new PieSeries<double> { Values = new double[] { double.TryParse(nutritionFact?.Fiber, out var fiber) ? fiber : 0 }, Name = "Fiber" },
+                new PieSeries<double> { Values = new double[] { double.TryParse(nutritionFact?.Protein, out var protein) ? protein : 0 }, Name = "Protein" }
+            };
         }
     }
 }
